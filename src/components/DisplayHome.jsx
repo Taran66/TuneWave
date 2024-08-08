88f643c2793182e8179b6120cda7bdb2c62e466b
Taran66 <taranpreet1911@gmail.com>
2024-08-08 14:05:59 +0530

    added new elements

---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar"
+import { albumsData } from "../assets/frontend-assets/assets"
+import AlbumItem from "./AlbumItem"
+
+const DisplayHome = () => {
+  return (
+    <>
+    <Navbar />
+    <div className="my-5 font-bold text-2xl">
+      {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+    </div>
+    </>
+  )
+}
+
+export default DisplayHome
