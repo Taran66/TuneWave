31a46dcfd3f7e2161a7f00f9d2b83c10fff197bf
Taran66 <taranpreet1911@gmail.com>
2024-08-12 19:33:28 +0530

    final commit of the day

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,3 +1,4 @@
+import DisplayAlbum from "./DisplayAlbum"
 import DisplayHome from "./DisplayHome"
 import { Route, Routes } from 'react-router-dom'
 
@@ -6,6 +7,7 @@ const Display = () => {
     <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
         <Routes>
             <Route path='/' element={<DisplayHome/>}/>
+            <Route path="/album/:id" element={<DisplayAlbum />}/>
         </Routes>
     </div>
   )
